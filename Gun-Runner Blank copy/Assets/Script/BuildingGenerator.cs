5553fe0fdc9531b6d7a97e2e4cbf0146d6836160
paul-herron <61231413+paul-herron@users.noreply.github.com>
2020-05-14 11:56:19 +0100

    Creative Coding exercise
    I'm currently attempting to make my city generator instantiate spires on a sphere, like a small planet, but I cannot get the spires to face away from the planet surface, instead they just face straight upwards. Alternatively, the spires can spawn on a flat plane using a commented out line of code in the CityBuilder script

---
+++ Gun-Runner Blank copy/Assets/Script/BuildingGenerator.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingGenerator : MonoBehaviour
+{
+    public int stories = 5;
+    public GameObject[] baseParts;
+    public GameObject[] middleParts;
+    public GameObject[] topParts;
+
+
+
+    void Start()
+    {
+        Build();
+    }
+
+
+    void Build()
+    {
+        float heightOffset = 0;
+        heightOffset += SpawnPieceLayer(baseParts, heightOffset);
+
+        for (int i = 2; i < stories; i++)
+        {
+           heightOffset += SpawnPieceLayer(middleParts, heightOffset);
+        }
+
+        SpawnPieceLayer(topParts, heightOffset);
+    }
+
+        float SpawnPieceLayer(GameObject[] pieceArray, float inputHeight)
+        {
+            Transform randomTransform = pieceArray[Random.Range(0, pieceArray.Length)].transform;
+            GameObject clone = Instantiate(randomTransform.gameObject, this.transform.position
+                + new Vector3(0, inputHeight, 0), transform.rotation) as GameObject;
+            Mesh cloneMesh = clone.GetComponentInChildren<MeshFilter>().mesh;
+            Bounds bounds = cloneMesh.bounds;
+            float heightOffset = bounds.size.y;
+
+            clone.transform.SetParent(this.transform);
+
+            return heightOffset;
+        
+    }
+}
---
+++ Gun-Runner Blank copy/Assets/Script/CityBuilder.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CityBuilder : MonoBehaviour
+{
+    public Transform Core;
+    public GameObject[] buildings;
+    public int mapHeight = 10;
+    public int mapWidth = 10;
+    public int buildingSpace = 5;
+    
+
+    void Start()
+    {
+        //Transform corePos = Core.transform;
+        float seed = Random.Range(0, 100);
+        for (int h = 0; h < mapHeight; h++)
+        {
+            for (int w = 0; w < mapWidth; w++)
+            {
+                float x = Mathf.Cos(360) * 100;
+                float z = Mathf.Sin(360) * 100;
+                int result = (int)(Mathf.PerlinNoise(w/10.0f + seed, h/10.0f + seed) * 10);
+                //Vector3 pos = new Vector3(w * buildingSpace, 0, h * buildingSpace);
+                Vector3 pos = Random.onUnitSphere  * 100;
+                if (result < 2)
+                {
+                    Instantiate(buildings[0], pos, Quaternion.identity);
+                    this.gameObject.transform.LookAt(Core);
+                }
+                else if (result < 4)
+                {
+                    Instantiate(buildings[1], pos, Quaternion.identity);
+                    this.gameObject.transform.LookAt(Core);
+                }
+                else if (result < 6)
+                {
+                    Instantiate(buildings[2], pos, Quaternion.identity);
+                    this.gameObject.transform.LookAt(Core);
+                }
+                else if (result < 8)
+                {
+                    Instantiate(buildings[3], pos, Quaternion.identity);
+                    this.gameObject.transform.LookAt(Core);
+                }
+                else if (result < 10)
+                {
+                    Instantiate(buildings[4], pos, Quaternion.identity);
+                    this.gameObject.transform.LookAt(Core);
+                }
+            }
+        }
+    }
+}
